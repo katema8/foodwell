c099741f4ef54713038ea0d07fa9a398e6dcce87
Raghu Betina <raghu@firstdraft.com>
2019-08-31 04:53:15 +0000

    Whitelist columns in WellnessLog for ActiveAdmin

--- app/admin/wellness_logs.rb
+++ app/admin/wellness_logs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register WellnessLog do
+
+ permit_params :gut_wellness, :date, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
